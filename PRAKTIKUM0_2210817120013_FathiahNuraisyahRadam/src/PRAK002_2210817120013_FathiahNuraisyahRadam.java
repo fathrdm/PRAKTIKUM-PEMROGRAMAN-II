39bc3f31479d7948939b58cc5b30d890169c5bf2
fathrdm <114979588+fathrdm@users.noreply.github.com>
2023-10-18 10:11:30 +0800

    Add files via upload

---
+++ PRAKTIKUM0_2210817120013_FathiahNuraisyahRadam/src/PRAK002_2210817120013_FathiahNuraisyahRadam.java
@@ -0,0 +1,10 @@
+public class PRAK002_2210817120013_FathiahNuraisyahRadam {
+    public static void main(String[] args){
+        System.out.println("###############################\n" +
+                "#                             #\n" +
+                "#   Fathiah Nuraisyah Radam   #\n" +
+                "#        2210817120013        #\n" +
+                "#                             #\n" +
+                "###############################");
+    }
+}
